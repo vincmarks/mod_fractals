ee98fb8ebc89857217aaaa2a866e670f57ae7d07
Helena <hschuh01@students.uni-mainz.de>
2026-09-02 14:28:41 +0200

    mehr drucklevel

--- issr_proc.jl
+++ issr_proc.jl
@@ -1,7 +1,6 @@
 # Code übersetzt aus Python
 include("func.jl")
 
-ds = NCDataset(joinpath(@__DIR__, "pl201001.nc"))
 ds = NCDataset(joinpath(@__DIR__, "pl201107.nc"))
 q = ds["q"][:, :, :, :]   # (1440, 721, 3, 124)
 t = ds["t"][:, :, :, :]   # same shape
@@ -14,6 +13,7 @@ time = ds["time"][:]
 #close(ds)
 
 rhi_200 = rhi_calc(q[:, :, 1, :], t[:, :, 1, :], 20000)
+rhi_250 = rhi_calc(q[:, :, 2, :], t[:, :, 2, :], 25000)
 rhi_300 = rhi_calc(q[:, :, 3, :], t[:, :, 3, :], 30000)
 
 
@@ -47,4 +47,3 @@ plot!(p; xlabel = "Wellenzahl k", ylabel = "Leistung S(k)",
       title = "rhi_300, Zeitschritt $t_idx")
 isdir("out") || mkdir("out")
 savefig(p, joinpath("out", "05_rhi300_spektrum.png"))
-
